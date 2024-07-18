ce432cf86b6babd2d7db6fd3304d7e7e65e2207c
Curtis Polic <curtis.polic@gmail.com>
2024-07-19 09:24:19 +1000

    enemy floating health bars

--- Objects/Enemy.cs
+++ Objects/Enemy.cs
@@ -9,13 +9,17 @@ public class Enemy : Drawable
 {
     public Vector2 Target;
     public List<Vector2> PathListToTarget;
+    public Texture2D HealthBarTexture, HealthBarBackTexture;
     public string Behaviour;
+    public int MaxHP, CurrentHP, MaxMP, CurrentMP;
     public Enemy(CoreGame parent, Vector2 screenCenter): base(parent, new(256,0), screenCenter)
     {
         Target = Position;
         PathListToTarget = new();
         Behaviour = "idle";
         Collision = true;
+        CurrentHP = 70;
+        MaxHP = 100;
         LoadContent();
     }
 
@@ -71,11 +75,20 @@ public class Enemy : Drawable
     public override void LoadContent()
     {
         Texture = GameParent.Content.Load<Texture2D>("goblin");
+        HealthBarTexture = GameParent.Content.Load<Texture2D>("health_bar");
+        HealthBarBackTexture = GameParent.Content.Load<Texture2D>("health_bar_back");
         base.LoadContent();
     }
 
     public override void Draw(SpriteBatch spriteBatch, Vector2 playerPos)
     {
+        // Backing for health bar
+        spriteBatch.Draw(HealthBarBackTexture, Position - new Vector2(0,32), null, Color.White, 0f, Offset + playerPos - ScreenCenter, Vector2.One, SpriteEffects.None, 0f);
+        
+        // Health bar
+        Rectangle healthPortion = new(new(0,0), new(64 * CurrentHP / MaxHP, 16));
+        spriteBatch.Draw(HealthBarTexture, Position - new Vector2(0,32), healthPortion, Color.White, 0f, Offset + playerPos - ScreenCenter, Vector2.One, SpriteEffects.None, 0f);
+        
         spriteBatch.Draw(Texture, Position, null, Color.White, 0f, Offset + playerPos - ScreenCenter, Vector2.One, SpriteEffects.None, 0f);
     }
 }
